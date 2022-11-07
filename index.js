bc1eeb25b7ab7e0eb0fdbcbc4246e68a5c42ac49
Marvin Lopez <lopez.marvin9@gmail.com>
2022-11-06 23:40:48 -0600

    manage request params

--- index.js
+++ index.js
@@ -12,9 +12,38 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000
+    },
+    {
+      name: 'Producto 2',
+      price: 2000
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  /*
+   * De esta forma se puede recibir el id
+   * (se debe recibir con el mismo nombre que se puso en el path)
+   *
+   * Ejemplo normal: const id = req.params.id;
+   */
+  const { id } = req.params;
   res.json({
+    id,
     name: 'Producto 1',
     price: 1000
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
