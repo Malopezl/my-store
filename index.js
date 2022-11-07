761376e54b77415419be9a0ce1d444b1b858e08b
Marvin Lopez <lopez.marvin9@gmail.com>
2022-11-06 23:17:33 -0600

    add custom routing

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Escuchando en el puerto: ${process.env.PORT}`);
 });
