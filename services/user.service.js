612e9f4ec1557e7dc1a63982e8d84b1e604bb52f
Marvin Lopez <lopez.marvin9@gmail.com>
2023-02-08 23:32:35 -0600

    add user schema & model

--- services/user.service.js
+++ services/user.service.js
@@ -1,34 +1,38 @@
-const pool = require('../libs/postgres.pool');
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
 
 class UserService {
 
   constructor() {
-    this.pool = pool;
-    this.pool.on('error', (err) => console.log(err));
   }
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const res = await pool.query(query);
-    return res.rows;
+    const users = await models.User.findAll();
+    return users;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 
