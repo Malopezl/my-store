4a4f8c87b7473c49ba957429915c13bad0c5e524
Marvin Lopez <lopez.marvin9@gmail.com>
2023-02-11 00:31:02 -0600

    change postgresql to mysql

--- config/config.js
+++ config/config.js
@@ -8,6 +8,10 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
+  mysqlName: process.env.MYSQLDATABASE,
+  mysqlPassword: process.env.MYSQLPASSWORD,
+  mysqlPort: process.env.MYSQLPORT,
+  mysqlUser: process.env.MYSQLUSER,
 }
 
 module.exports = { config };
