825010b9dd9442d4045e935755fc2f21f172831f
Marvin Lopez <lopez.marvin9@gmail.com>
2023-02-12 00:15:30 -0600

    generate migrations to add columns to a table

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,10 +2,10 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const setupModels = require('./../db/models');
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const URI = `postgres://${config.mysqlUser}:${config.mysqlPassword}@${config.dbHost}:${config.mysqlPort}/${config.mysqlName}`;
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `mysql://${config.mysqlUser}:${config.mysqlPassword}@${config.dbHost}:${config.mysqlPort}/${config.mysqlName}`;
 
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
