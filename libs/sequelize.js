7eb82ff2347c545ab549c188bb22dc1cdc7d657d
Marvin Lopez <lopez.marvin9@gmail.com>
2023-02-12 00:01:49 -0600

    generate basic migrations for users

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,14 +5,14 @@ const setupModels = require('./../db/models');
 // const USER = encodeURIComponent(config.dbUser);
 // const PASSWORD = encodeURIComponent(config.dbPassword);
 // const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const URI = `mysql://${config.mysqlUser}:${config.mysqlPassword}@${config.dbHost}:${config.mysqlPort}/${config.mysqlName}`;
+const URI = `postgres://${config.mysqlUser}:${config.mysqlPassword}@${config.dbHost}:${config.mysqlPort}/${config.mysqlName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: console.log,
 });
 
 setupModels(sequelize);
-sequelize.sync();
+// sequelize.sync();
 
 module.exports = sequelize;
